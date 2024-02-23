e780d371afe2ec35ba30f2e305f044960106421d
Hector <58075182+hectorm926@users.noreply.github.com>
2024-02-23 20:33:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 <script>
+<p id="countdown"></p>  
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 1, 2025 00:00:00").getTime();
+var countDownDate = new Date("Apr 1, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,7 +18,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+  // Display the result in the element with id="countdown"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
